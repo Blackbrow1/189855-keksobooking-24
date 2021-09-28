463b43ede8374408e408c89e62f29ab2ea9ace72
Alexander <alesandro83@mail.ru>
2021-09-28 21:52:31 +0300

    Добавил вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function getRandomIntInclusive(min, max) {
+  if (min >= max) {
+    max = Math.ceil(max);
+    min = Math.floor(min);
+    return Math.floor(Math.random() * (min - max + 1)) + max;
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRandomIntInclusive(1, 10);
+
+function getRandomArbitrary(min, max) {
+  if (min >= max && max || min > 0) {
+    return Math.random() * (min - max) + max;
+
+  } else if (max >=min && max || min > 0) {
+    return Math.random() * (max - min) + min;
+  }
+
+  return 'Число должно быть больше 0';
+}
+
+getRandomArbitrary(1, 10).toFixed(20);
+
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
