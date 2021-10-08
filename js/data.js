853b86cd6cba70852730fc02a46c1f1831fd332b
Alexander <alesandro83@mail.ru>
2021-10-08 19:01:48 +0300

    Модуляция

---
+++ js/data.js
@@ -0,0 +1,49 @@
+const TITLES = [
+  'Просторная квартира в центре Токио',
+  'Замечательная комната с великолепным видом на горы',
+  'Уютная квартира для студентов',
+  'Та самая квартира, о которой вы мечтали',
+];
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const CHECKOUTS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const DESCRIPTIONS = [
+  'Помещение находится всего в пяти минутах ходьбы от метро',
+  'Просторные, светлые комнаты будут радовать вас каждый день',
+  'Стиральная машина, микроволновка, диван и утюг - всё в Вашем распоряжении!',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+export {TITLES, TYPES, CHECKINS, CHECKOUTS, FEATURES, DESCRIPTIONS, PHOTOS};
